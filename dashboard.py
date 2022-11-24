4056ae538846a7139e307105ddb469e405e856ad
Madeleine Larson <madeleinelarson343@yahoo.com>
2022-11-23 20:41:39 -0800

    starting dash

--- dashboard.py
+++ dashboard.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pandas as pd
 from multiapp import MultiApp
-from apps import home, data, model
+from apps import display, home, predict
 
 app = MultiApp()
 st.title('Baseball College Athlete Dashboard')
 app.add_app("Homepage", home.app)
-app.add_app("Display", data.app)
-app.add_app("Predict", model.app)
+app.add_app("Display", display.app)
+app.add_app("Predict", predict.app)
 
 app.run()
